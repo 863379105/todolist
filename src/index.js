53984f5428e8b0d1212dc60c2bc10d93480d0e7b
lb <863379105@qq.com>
2022-04-05 22:41:59 +0800

    fit react 18
    Signed-off-by: lb <863379105@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import TodoList from './TodoList';
 
-ReactDOM.render(
-  <TodoList />,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'))
+root.render(<TodoList />);
